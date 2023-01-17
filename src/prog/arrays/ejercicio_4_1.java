95d098a92feaa654a44a9a7a247daf306af78f9b
atelper1005 <atelper1005@g.educaand.es>
2023-01-17 18:18:07 +0100

    commit 1
    commit1

---
+++ src/prog/arrays/ejercicio_4_1.java
@@ -0,0 +1,23 @@
+package prog.arrays;
+
+public class ejercicio_4_1 {
+    public static void main(String[] args) {
+        int x = 1;
+        String s = "Juan";
+        metodo(x, s);
+
+
+        System.out.println(metodo(x, s));
+    }
+
+    private static void metodo () {
+    }
+
+    private static int metodo(int x) {
+        return x;
+    }
+
+    private static String metodo(int x, String s) {
+        return s;
+    }
+}
